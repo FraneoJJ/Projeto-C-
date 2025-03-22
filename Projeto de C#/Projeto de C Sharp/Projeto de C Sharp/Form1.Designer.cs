03521296a58f864b2bc1a432ee6e5f6b5042bb73
Frâneo <133277824+FraneoJoseJoao@users.noreply.github.com>
2025-03-22 17:19:07 +0100

    Ficheiros do projeto

---
+++ Projeto de C#/Projeto de C Sharp/Projeto de C Sharp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Projeto_de_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1248, 605);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_import
+            // 
+            this.btn_import.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_import.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_import.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_import.Location = new System.Drawing.Point(101, 676);
+            this.btn_import.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(175, 58);
+            this.btn_import.TabIndex = 1;
+            this.btn_import.Text = "Importar";
+            this.btn_import.UseVisualStyleBackColor = true;
+            this.btn_import.Click += new System.EventHandler(this.btn_import_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(304, 676);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 676);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(527, 676);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(193, 58);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1281, 839);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_import);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ Projeto de C#/Projeto de C Sharp/Projeto de C Sharp/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;// Para DataTable
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Excel = Microsoft.Office.Interop.Excel;// Cria um alias para Excel
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.Data.SqlClient;
+
+namespace Projeto_de_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        string caminho = "server=localhost; user=root; password=; database=excel;";
+        System.Data.DataTable tabela = new System.Data.DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrirDOC = new OpenFileDialog();
+
+            try
+            {
+                abrirDOC.Filter = "Excel Files|*.xlsx";
+                if (abrirDOC.ShowDialog() == DialogResult.OK)
+                {
+                    string arquivo = abrirDOC.FileName;
+                    enviar(arquivo);
+            }
+
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show($"Falaha ao carregar o arquivo; Erro: {erro}");
+            }
+            
+        }
+
+        private void enviar(string localAqui)
+        {
+    
+        try
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(localAqui);
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+            Excel.Range range = worksheet.UsedRange;
+
+           // Usado para DataTable
+           
+
+            // Criar colunas no DataTable
+            for (int col = 1; col <= range.Columns.Count; col++)
+            {
+                string colName = range.Cells[1, col].Value2?.ToString() ?? $"Coluna {col}";
+                tabela.Columns.Add(colName);
+            }
+
+            // Preencher as linhas do DataTable
+            for (int row = 2; row <= range.Rows.Count; row++)
+            {
+                DataRow dataRow = tabela.NewRow();
+                for (int col = 1; col <= range.Columns.Count; col++)
+                {
+                    dataRow[col - 1] = range.Cells[row, col].Value2?.ToString() ?? string.Empty;
+                }
+                tabela.Rows.Add(dataRow);
+            }
+
+            // Liberar recursos
+            workbook.Close(false);
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+            // Exibir no DataGridView
+            dataGridView1.DataSource = tabela;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Verifique se tem o pacote office instalado no seu computador; Erro ao carregar o Excel: {ex.Message}");
+        }
+    }
+
+        private void InserirDadosNoBanco()
+        {
+            try
+            {
+                // String de conexão com o banco de dados SQL Server
+                string conexaoString = @"Data Source=SEU_SERVIDOR;Initial Catalog=SEU_BANCO;Integrated Security=True";
+
+                using (SqlConnection conexao = new SqlConnection(conexaoString))
+                {
+                    conexao.Open();
+
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (!row.IsNewRow)  // Ignora a linha de edição vazia
+                        {
+                            string comandoSQL = "INSERT INTO TabelaExemplo (Coluna1, Coluna2, Coluna3) VALUES (@valor1, @valor2, @valor3)";
+
+                            using (SqlCommand comando = new SqlCommand(comandoSQL, conexao))
+                            {
+                                // Adicionando parâmetros para evitar SQL Injection
+                                comando.Parameters.AddWithValue("@valor1", row.Cells[0].Value ?? DBNull.Value);
+                                comando.Parameters.AddWithValue("@valor2", row.Cells[1].Value ?? DBNull.Value);
+                                comando.Parameters.AddWithValue("@valor3", row.Cells[2].Value ?? DBNull.Value);
+
+                                comando.ExecuteNonQuery();
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Dados inseridos com sucesso!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao inserir dados no banco: {ex.Message}");
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection(caminho);
+            conexao.Open();
+            MySqlCommand comando = new MySqlCommand("alter table aluno add column @nome_coluna", conexao);
+            comando.Parameters.AddWithValue("@nome_coluna", dataGridView1.Rows);
+            conexao.Close();
+        }
+    }
+}
